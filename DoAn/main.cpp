d7fa4768312b3dde84496c6fa7d5b5481511db12
dzungvd98 <yungvd98@gmail.com>
2024-11-21 19:44:24 +0700

    update log

--- DoAn/main.cpp
+++ DoAn/main.cpp
@@ -7,16 +7,24 @@ using namespace std;
 int main()
 {	
 	//---------------HAI----------------
+<<<<<<< HEAD
 	//Page* p = new Page();
 	//p->setRun(PAGE_LOADING);
 	//p->main();
 	//delete p;
 	//----------------------------------
+=======
+	Page* p = new Page();
+	p->setRunPage(PAGE_LOADING);
+	p->main();
+	delete p;
+	//----------p;------------------------
+>>>>>>> cbff50688a88d1d1a38e063e534e2d97f0302792
 
 	//----------------HUY ----------------
-	/*HuyTest* h = new HuyTest();
-	h->main();
-	delete h;*/
+	//HuyTest* h = new HuyTest();
+	//h->main();
+	//delete h;
 	//------------------------------------------
 	
 	//--------------------DUNG-------------------
