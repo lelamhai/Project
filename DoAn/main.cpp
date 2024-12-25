093084a8fc5450bb73ab7ddc86bfa82fa6930ebc
ADMIN\ADMIN <leelamhair@gmail.com>
2024-12-25 10:24:57 +0700

    reset commit of Dung

--- DoAn/main.cpp
+++ DoAn/main.cpp
@@ -1,4 +1,4 @@
-#include "Page.h"
+﻿#include "Page.h"
 #include "HuyTest.h"
 #include "DungTest.h"
 
@@ -7,19 +7,11 @@ using namespace std;
 int main()
 {	
 	//---------------HAI----------------
-//<<<<<<< HEAD
-	//Page* p = new Page();
-	//p->setRun(PAGE_LOADING);
-	//p->main();
-	//delete p;
-	//----------------------------------
-//=======
 	Page* p = new Page();
 	p->setRunPage(PAGE_LOADING);
 	p->main();
 	delete p;
-	//----------p;------------------------
-//>>>>>>> cbff50688a88d1d1a38e063e534e2d97f0302792
+	//----------------------------------
 
 	//----------------HUY ----------------
 	//HuyTest* h = new HuyTest();
